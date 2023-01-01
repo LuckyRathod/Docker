9afa9cccc2be92615cefb36cfb58a5b840e9fa5b
LuckyRathod <luckyrathod46@gmail.com>
2023-01-01 16:31:23 +0530

    Docker Compose and Imp Commands
    Docker Compose and Imp commands

---
+++ 2.Docker-File-Sample/app.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return "My First Image"
+
+@app.route('/container')
+def hello():
+    return 'container working successfully' 
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
--- Custom_Image/Env_variable/app.py
+++
@@ -1,7 +0,0 @@
-import os
-
-username = os.environ['MY_USER']
-password = os.environ['MY_PASS']
-
-print(f"Running with user {username}")
-print(f"Running with password {password}")
